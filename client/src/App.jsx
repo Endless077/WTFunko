e736aafcf2dc2b12b8244b549828dd974ca4f4f4
JewDaiko <m.palmisciano1@studenti.unisa.it>
2024-05-15 23:00:50 +0200

    Dummy Front-end with fake-api

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import {Navbar} from "./components/Navbar.jsx";
+import {Home} from "./Home.jsx";
+
+function App() {
+
+  return (
+      <>
+      <Navbar/>
+      <Home/>
+      </>
+  )
+}
+
+export default App
